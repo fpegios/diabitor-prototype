4a898d66fe3a0f652664c2d330e6588a2879636b
Fotis Pegios <fotispeg@gmail.com>
2018-02-14 11:50:15 +0100

    basic navigation through pages

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
-console.log('a log msg from main.js');
+$( "#open-monitor-btn" ).click(function() {
+    hide(".login");
+    show(".monitor");
+});
+
+$( "#open-login-btn" ).click(function() {
+    hide(".monitor");
+    show(".login");
+});
+
+function show(element){
+    $( element).removeClass("hidden");
+    $( element).addClass("visible");
+}
+
+function hide(element){
+    $( element).removeClass("visible");
+    $( element).addClass("hidden");
+}
