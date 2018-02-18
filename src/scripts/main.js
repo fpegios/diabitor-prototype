0ccfe0d5145c69d9dc5bfecdd36c01d0c8e97c02
Fotis Pegios <fotispeg@gmail.com>
2018-02-18 01:39:33 +0100

    IMPORTANT refactoring
    -- data-include => data-component
    -- use class hidden and actrve in index file
    -- src/views/ => src/components/
    -- src/styles/pages => src/styles/components
    -- component reference => "[data-component="NAME_OF_COMPONENT"]
    -- use showComponent("NAME_OF_COMPONENT") function to show a component
    -- scss file for each component must start with the component reference
    [data-component="NAME_OF_COMPONENT"]{
    // scss code here
    }
    -- the scss files for regarding components are imported in app.scss

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,114 +8,14 @@ $(window).on('load', function() {
     hidePreloader();
 });
 
-// include all html components
-function includeComponents() {
-    var includes = $('[data-include]');
-    $.each(includes, function(){
-        var file = 'src/views/' + $(this).data('include') + '.html';
+// generate html components
+function includeComponents() { 
+    var components = $('[data-component]');
+    $.each(components, function(){
+        var file = 'src/components/' + $(this).data('component') + '.html';
         $(this).load(file, function() {
             initClickEvents();
         });
-    });
-}
-
-// initialize click events
-function initClickEvents() {
-    // HEADER
-    // 
-    $( ".measurement .left-action-btn" ).click(function() {
-        show(".monitor");
-    });
-    $( ".history-measurement .left-action-btn" ).click(function() {
-        show(".history");
-        historyMeasurement = 0;
-        $( ".history-measurement .nav-arrow.prev" ).hide();
-    });
-    $( ".parent-history-measurement .left-action-btn" ).click(function() {
-        show(".parent-history");
-        parentHistoryMeasurement = 0;
-        $( ".parent-history-measurement .nav-arrow.prev" ).hide();
-    });
-
-    // BODY
-    // 
-    $( "#kid-mode" ).click(function() {
-        show(".monitor");
-    });
-    $( "#parent-mode" ).click(function() {
-        show(".parent-status");
-    });
-    
-    $( "#monitor-btn" ).click(function() {
-        show(".measurement");
-    });
-    $( ".history .meas-1" ).off().click(function() {
-        historyMeasurement = 0;
-        updateHistoryMeasurement("kid", historyMeasurement);
-        show(".history-measurement");
-    });
-    $( ".history .meas-2" ).off().click(function() {
-        historyMeasurement = 1;
-        updateHistoryMeasurement("kid", historyMeasurement);
-        show(".history-measurement");
-    });
-    $( ".history .meas-3" ).off().click(function() {
-        historyMeasurement = 2;
-        updateHistoryMeasurement("kid", historyMeasurement);
-        show(".history-measurement");
-    });
-    $( ".history-measurement .nav-arrow.prev" ).off().click(function() {
-        historyMeasurement = historyMeasurement - 1;
-        updateHistoryMeasurement("kid", historyMeasurement);
-    });
-    $( ".history-measurement .nav-arrow.next" ).off().click(function() {
-        historyMeasurement = historyMeasurement + 1;
-        updateHistoryMeasurement("kid", historyMeasurement);
-    });
-    $( ".parent-history .meas-1" ).off().click(function() {
-        parentHistoryMeasurement = 0;
-        updateHistoryMeasurement("parent", parentHistoryMeasurement);
-        show(".parent-history-measurement");
-    });
-    $( ".parent-history .meas-2" ).off().click(function() {
-        parentHistoryMeasurement = 1;
-        updateHistoryMeasurement("parent", parentHistoryMeasurement);
-        show(".parent-history-measurement");
-    });
-    $( ".parent-history .meas-3" ).off().click(function() {
-        parentHistoryMeasurement = 2;
-        updateHistoryMeasurement("parent", parentHistoryMeasurement);
-        show(".parent-history-measurement");
-    });
-    $( ".parent-history-measurement .nav-arrow.prev" ).off().click(function() {
-        parentHistoryMeasurement = parentHistoryMeasurement - 1;
-        updateHistoryMeasurement("parent", parentHistoryMeasurement);
-    });
-    $( ".parent-history-measurement .nav-arrow.next" ).off().click(function() {
-        parentHistoryMeasurement = parentHistoryMeasurement + 1;
-        updateHistoryMeasurement("parent", parentHistoryMeasurement);
-    });
-    
-    // FOOTER
-    //
-    $( ".monitor-tab" ).click(function() {
-        show(".monitor");
-    });
-    
-    $( ".history-tab" ).click(function() {
-        show(".history");
-    });
-    
-    $( ".profile-tab" ).click(function() {
-        show(".profile");
-    });
-    
-    $( ".parent-status-tab" ).click(function() {
-        show(".parent-status");
-    });
-    
-    $( ".parent-history-tab" ).click(function() {
-        show(".parent-history");
     });
 }
 
@@ -127,10 +27,10 @@ function hidePreloader() {
     });
 }
 
-// hide active element and show the selected one
-function show(element){
+// hide the active component and show the selected
+function showComponent(component) {
     $(".active").removeClass("active").addClass("hidden");
-    $( element).removeClass("hidden").addClass("active");
+    $(getDataComponent(component)).addClass("active").removeClass("hidden");
 }
 
 // initialize variables
@@ -143,35 +43,40 @@ function initVariables() {
 function updateHistoryMeasurement(mode, id) {
     if (mode == "kid") {
         if (id == 0) {
-            $( ".history-measurement .nav-arrow.prev" ).hide();
-            $( ".history-measurement .nav-arrow.next" ).show();
+            $( getDataComponent("history-measurement") + " .nav-arrow.prev" ).hide();
+            $( getDataComponent("history-measurement") + " .nav-arrow.next" ).show();
         } else if (id == 1) {
-            $( ".history-measurement .nav-arrow.prev" ).show();
-            $( ".history-measurement .nav-arrow.next" ).show();
+            $(getDataComponent("history-measurement") + " .nav-arrow.prev" ).show();
+            $(getDataComponent("history-measurement") + " .nav-arrow.next" ).show();
         } else if (id == 2) {
-            $( ".history-measurement .nav-arrow.prev" ).show();
-            $( ".history-measurement .nav-arrow.next" ).hide();
+            $( getDataComponent("history-measurement") + " .nav-arrow.prev" ).show();
+            $( getDataComponent("history-measurement") + " .nav-arrow.next" ).hide();
         }
-        $( ".history-measurement .glucose-bottle img").attr('src', measurements[id].img);
-        $( ".history-measurement .message").html(measurements[id].message);
-        $( ".history-measurement .value").html(measurements[id].value);
-        $( ".history-measurement .date").html(measurements[id].date);
-        $( ".history-measurement .day").html(measurements[id].day);
+        $( getDataComponent("history-measurement") + " .glucose-bottle img").attr('src', measurements[id].img);
+        $( getDataComponent("history-measurement") + " .message").html(measurements[id].message);
+        $( getDataComponent("history-measurement") + " .value").html(measurements[id].value);
+        $( getDataComponent("history-measurement") + " .date").html(measurements[id].date);
+        $( getDataComponent("history-measurement") + " .day").html(measurements[id].day);
     } else {
         if (id == 0) {
-            $( ".parent-history-measurement .nav-arrow.prev" ).hide();
-            $( ".parent-history-measurement .nav-arrow.next" ).show();
+            $( getDataComponent("parent-history-measurement") + " .nav-arrow.prev" ).hide();
+            $( getDataComponent("parent-history-measurement") + " .nav-arrow.next" ).show();
         } else if (id == 1) {
-            $( ".parent-history-measurement .nav-arrow.prev" ).show();
-            $( ".parent-history-measurement .nav-arrow.next" ).show();
+            $( getDataComponent("parent-history-measurement") + " .nav-arrow.prev" ).show();
+            $( getDataComponent("parent-history-measurement") + " .nav-arrow.next" ).show();
         } else if (id == 2) {
-            $( ".parent-history-measurement .nav-arrow.prev" ).show();
-            $( ".parent-history-measurement .nav-arrow.next" ).hide();
+            $( getDataComponent("parent-history-measurement") + " .nav-arrow.prev" ).show();
+            $( getDataComponent("parent-history-measurement") + " .nav-arrow.next" ).hide();
         }
-        $( ".parent-history-measurement .glucose-bottle img").attr('src', measurements[id].img);
-        $( ".parent-history-measurement .message").html(measurements[id].message);
-        $( ".parent-history-measurement .value").html(measurements[id].value);
-        $( ".parent-history-measurement .date").html(measurements[id].date);
-        $( ".parent-history-measurement .day").html(measurements[id].day);
+        $( getDataComponent("parent-history-measurement") + " .glucose-bottle img").attr('src', measurements[id].img);
+        $( getDataComponent("parent-history-measurement") + " .message").html(measurements[id].message);
+        $( getDataComponent("parent-history-measurement") + " .value").html(measurements[id].value);
+        $( getDataComponent("parent-history-measurement") + " .date").html(measurements[id].date);
+        $( getDataComponent("parent-history-measurement") + " .day").html(measurements[id].day);
     }
+}
+
+// get component reference value
+function getDataComponent(component) {
+    return "[data-component=" + component + "]";
 }
